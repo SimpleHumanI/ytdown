c8aca47e60abe54d0b62c88c4bfc28330e102fd4
mast <kazemalimmd@gmail.com>
2025-01-18 20:53:10 +0330

    add new arguments

--- ytdown.py
+++ ytdown.py
@@ -1,18 +1,26 @@
 #! /bin/python3
+"""
+    this program uses y2down.cc api for handle
+    youtube links 
+"""
 
 from tqdm import tqdm
 from requests import get
+from re import match
 from argparse import ArgumentParser
+
 global urls
 urls = list()
 
-class handle_args:
+class handle_args():
     def __init__(self):
         self.parse = ArgumentParser(description="download single/multiple media from youtube")
+        self.parse.add_argument("url", nargs="?")
         self.parse.add_argument("-f", "--file", help="file with multiple url separated with newline")
-        self.parse.add_argument("-u", "--url", help="single url to download media")
-        self.parse.add_argument("-o", "--output", help="file path and file name")
-        self.argv = self.parse.parse_args()
+        self.parse.add_argument("-F", "--format", help="mp4 qualities and other formats: (360,480,720,1080), mp3, m4a, webm, wav, ogg, flac, opus")
+        self.parse.add_argument("-n", "--filename", help="specific file name except youtube video title")
+
+        self.argv, self.argv_unknown = self.parse.parse_known_args()
 
     def no_arg_passed(self):
         values = vars(self.argv).values()
@@ -21,39 +29,38 @@ class handle_args:
             if value != None:
                 no_arg = False
         return no_arg
-
+    
     def help_page(self):
         self.parse.print_help()
         exit (1)
 
 class ytdown:
-    def __init__(self, content, _type):
-      #  self.urls = []
+    def __init__(self, url, _type, file_format="360", file_name:str=''):
+        self.filename = file_name 
 
         if _type == "url":
-            self.single_url(content)
-        if _type == "output":
+            self.single_url(url, file_format)
+        if _type == "filename":
             if len(urls) != 0 :
-                self.write_single_file(content)
+                self.write_single_file(url, file_format)
         elif _type == "file":
-            file_content = read_file(content)
-            self.multi_url(content)
+            file_content = read_file(url)
+            self.multi_url(url)
         
-    def single_url(self, url):
+    def single_url(self, url, file_format):
         """
         converting url with conv_url() function 
-        then show progress in progress bar to
-        user
+        then show progress in progress bar to user
         """
-        compared_url = self.conv_url(url)
+        compared_url = self.conv_url(url, file_format)
         
-        with tqdm(total=(10000), ncols=100) as stat_bar:
+        with tqdm(total=(100), ncols=100) as stat_bar:
             old_stat = 0
             while True: 
                 download_url = get(compared_url).json()
                 stat = download_url["progress"]
                 text = download_url["text"]
-                stat_bar.set_description("{}% | status : {}".format(stat, text))
+                stat_bar.set_description("status : {}".format(text))
                 
                 if download_url["success"] == 1:
                     stat_bar.total = stat_bar.n
@@ -62,49 +69,50 @@ class ytdown:
                         stat_bar.update(stat)
         urls.append(download_url["download_url"])
     
-    def multi_url(self, urls):
-        pass 
-
     def read_file(self):
         pass
 
-    def write_single_file(self, filename):
-        """
-        write to single file my postfix
-        mp3 for now . other formats not 
-        supported yet!
-        """
+    def write_single_file(self, file_format, filename=''):
+        if file_format in ("360", "480", "720", "1080"):
+            file_format = "mp4"
+        if not filename:
+            filename = self.filename
+
         file_content = get(urls[0]).content
-        with open(filename + ".mp3", "wb") as file:
+        filename = filename.replace(" ", "_")
+        with open(filename + "." + file_format, "wb") as file:
             file.write(file_content)
 
-    def conv_url(self, url):
+    def conv_url(self, url, file_format):
         """
         convert youtube url to downloadable file link
-        note : using y2down.cc api
         """
-        compare_url = f"https://loader.to/ajax/download.php?format=mp3&url={url}"
-        _id  = get(compare_url).json()["id"]
+        if not file_format in ("360", "480", "720", "1080", "mp3", "m4a", "webm", "wav", "ogg", "flac", "opus"):
+            print("wrong file format, you should pass one of valid formats to the -F option")
+            print("valid formats: 360, 480, 720, 1080, mp3, m4a, webm, wav, ogg, flac, opus")
+            exit(1)
+
+        compare_url = f"https://loader.to/ajax/download.php?format={file_format}&url={url}"
+        req = get(compare_url).json()
+        _id  = req["id"]
+        if not self.filename:
+            self.filename = req["title"] 
+         
         compare_id_link = f"https://p.oceansaver.in/ajax/progress.php?id={_id}"
         return compare_id_link
+
+    def is_link_valid(self, link):
+        check = re.compile(r'^(https?://)?(www\.)?(youtube\.com/watch\?v=|youtu\.be/|youtube\.com/embed/|youtube\.com/v/)([a-zA-Z0-9_-]{11})$')
+        return bool(check.match(link))
 
 if __name__ == "__main__":
     arguments = handle_args()
 
-    if arguments.no_arg_passed():
+    if arguments.no_arg_passed() or not arguments.argv.url:
         arguments.help_page()
     
-    if arguments.argv.file != None:
-        youtube = ytdown(arguments.argv.file, "file")
-    elif arguments.argv.url != None:
-        youtube = ytdown(arguments.argv.url, "url")
-    
-    if arguments.argv.output != None:
-        print(f"make {arguments.argv.output}.mp3 file...",end='')
-        ytdown(arguments.argv.output, "output")
-        print("\ndone")
-    else:
-        for i in urls:
-            print("----\ndownload link:", i)
+    youtube = ytdown(arguments.argv.url, "url",
+                     arguments.argv.format, arguments.argv.filename)
+    youtube.write_single_file(arguments.argv.format)
 
 
